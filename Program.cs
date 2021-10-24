8bacfdf164c48791114c20cbea0c0337aead86b1
Aga Nowak <agaenowak@poczta.fm>
2021-10-24 19:33:50 +0200

    Add project files

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace EvenAndOdd
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+
+            Console.WriteLine("Podaj liczbę całkowitą");
+            var number1 = GetNumber();
+            var number2 = number1 % 2;
+            Console.WriteLine(number2);
+
+            if (number2 == 0)
+            {
+                Console.WriteLine("Liczba jest parzysta");
+            }
+            else
+            {
+                Console.WriteLine("Liczba jest nieparzysta");
+            }
+        }
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int Number))
+                {
+                    Console.WriteLine("Nieprawidłowy format. Podaj inną liczbę");
+                    continue;
+                }
+                return Number;
+            }
+
+        }
+
+    }
+}
+
+
+
+
+
